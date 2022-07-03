e1968531f5290f94678c776591424d9d6deebaf6
Andrew Hunt <andrew.hunt@team.telstra.com>
2022-07-03 15:36:13 +0800

    Try different path to the framework path. (exclude .git)

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "Cancer200Api",
-         url: "https://github.com/andrewmapimagery/Cancer200Api.git/Cancer200Api.xcframework.zip",
+         url: "https://github.com/andrewmapimagery/Cancer200Api/Cancer200Api.xcframework.zip",
          checksum: "e7f75cb009eb4a1b8c56a1268b034f2c89863f6a80de6dd9254c1aaa768991e4"),
     ]
 )
