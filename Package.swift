cbfe28af850cc0c5125b50dc2dd830d22c18d1f7
Andrew Hunt <andrew.hunt@team.telstra.com>
2022-07-03 18:27:13 +0800

    Change name of zip file  again.

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "Cancer200Api",
-         url: "https://github.com/andrewmapimagery/Cancer200Api/cancer200api-xcframework.zip",
+         url: "https://github.com/andrewmapimagery/Cancer200Api/Cancer200api.xcframework.zip",
          checksum: "5fea79a480ea39de725e058ca547357fa87e672d49d7d178691b0dfaab0af863")
     ]
 )
