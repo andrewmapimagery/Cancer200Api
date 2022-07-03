8528d8343896af66fb874b286940661061a00b2d
Andrew Hunt <andrew.hunt@team.telstra.com>
2022-07-03 16:01:29 +0800

    Try zip file without symbols in it.

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "Cancer200Api",
-         url: "https://github.com/andrewmapimagery/Cancer200Api.git/Cancer200Api.xcframework.zip",
-         checksum: "e7f75cb009eb4a1b8c56a1268b034f2c89863f6a80de6dd9254c1aaa768991e4"),
+         url: "https://github.com/andrewmapimagery/Cancer200Api/Cancer200Api.xcframework.zip",
+         checksum: "5fea79a480ea39de725e058ca547357fa87e672d49d7d178691b0dfaab0af863"),
     ]
 )
