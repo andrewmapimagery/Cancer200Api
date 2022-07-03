875441cb40b456414782ae7fd2be5aa197e87a23
Andrew Hunt <andrew.hunt@team.telstra.com>
2022-07-03 14:50:50 +0800

    Fix up dependencies in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Cancer200Api",
     platforms: [
-        ..iOS(.v12)
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -15,8 +15,18 @@ let package = Package(
             targets: ["Cancer200Api"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/pkluz/PKHUD.git", 
+            from: "5.4.0"
+        ),
+        .package(
+            url: "https://github.com/SnapKit/SnapKit.git", 
+            from: "5.6.0"
+        ),
+        .package(
+            url: "https://github.com/malcommac/SwiftLocation.git", 
+            from: "5.1.0"
+        )
     ],
     targets: [
         .binaryTarget(name: "Cancer200Api",
