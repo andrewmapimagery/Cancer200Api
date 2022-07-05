9abd9d71641e3e1d47e973a80373018800a821d8
Andrew Hunt <andrew.hunt@team.telstra.com>
2022-07-05 20:32:31 +0800

    Update app to work with latest SPM module.

--- Tests/SdkTestNoStoryboard/AppDelegate.swift
+++ Tests/SdkTestNoStoryboard/AppDelegate.swift
@@ -14,12 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Cancer200.initApi()
         window = UIWindow()
         let viewController = MapViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
-        Cancer200.initApi()
         return true
     }
 }
